b011e51f4b304e1f7cfc4ea4bc6b88c682ecc982
TheRealPad <pierre-alexandre.delgado-arevalo@epitech.eu>
2024-01-31 20:27:13 -0800

    style(splash icon): add owl icon
    - remove useless import

--- app/src/main/java/com/eipsaferoad/owl/presentation/home/Home.kt
+++ app/src/main/java/com/eipsaferoad/owl/presentation/home/Home.kt
@@ -1,15 +1,11 @@
 package com.eipsaferoad.owl.presentation.home
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Favorite
-import androidx.compose.material.icons.rounded.Settings
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -17,10 +13,8 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.wear.compose.material.Icon
-import androidx.wear.compose.material.MaterialTheme
 import androidx.wear.compose.material.Text
 import com.eipsaferoad.owl.presentation.theme.OwlTheme
 
